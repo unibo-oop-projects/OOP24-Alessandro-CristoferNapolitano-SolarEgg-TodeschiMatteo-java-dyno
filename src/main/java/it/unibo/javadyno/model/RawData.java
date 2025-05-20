698e3a38735fb5a3458921f338b0e5aeae1a06f6
Alessandro <alessandroporcheddu000@gmail.com>
2025-05-20 22:20:42 +0200

    created rawdata interface

---
+++ src/main/java/it/unibo/javadyno/model/RawData.java
@@ -0,0 +1,12 @@
+package it.unibo.javadyno.model;
+
+/**
+ * RawData interface.
+ */
+public interface RawData {
+    /**
+     * Get a immutable copy of the data.
+     * @return
+     */
+    public RawData getData();
+}
