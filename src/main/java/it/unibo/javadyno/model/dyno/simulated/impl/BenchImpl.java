c9c1a000ef006f93b1d713d52b184920e3a69a3b
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-05-20 22:24:51 +0200

    Added Simulated Bench Implementation

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/BenchImpl.java
@@ -0,0 +1,45 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import it.unibo.javadyno.model.dyno.simulated.api.Bench;
+
+/**
+ * Simulated bench implementation.
+ */
+public class BenchImpl implements Bench {
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public int getRollerRPM() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRollerRPM'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public double getRollerAngularSpeed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRollerAngularSpeed'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public double getRollerInertia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRollerInertia'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public double getRollerRadius() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRollerRadius'");
+    }
+}
