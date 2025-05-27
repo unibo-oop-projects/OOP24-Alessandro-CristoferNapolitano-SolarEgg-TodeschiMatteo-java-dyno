3b427485d181ba6efea7804616cd57ece178a568
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-05-27 19:43:04 +0200

    Added implementation of SimulatedDyno

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/SimulatedDynoImpl.java
@@ -0,0 +1,45 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import it.unibo.javadyno.model.dyno.simulated.api.SimulatedDyno;
+
+/**
+ * Implementation of the simumlated Dyno.
+ */
+public class SimulatedDynoImpl implements SimulatedDyno {
+
+    /**
+     * @inheritdoc
+     */
+    @Override
+    public int getEngineRPM() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEngineRPM'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public int getRollerRPM() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRollerRPM'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public double getEngineTemp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEngineTemp'");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public double getLoadCellValue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLoadCellValue'");
+    }
+}
