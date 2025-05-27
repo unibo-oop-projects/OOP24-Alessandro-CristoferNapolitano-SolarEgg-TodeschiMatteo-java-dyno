5955bc169ae9959e02d84722104d1b28f2449e02
Alessandro <alessandroporcheddu000@gmail.com>
2025-05-27 18:46:20 +0200

    initialized datatransreciever

--- src/main/java/it/unibo/javadyno/model/data/api/DataTransreciever.java
+++ src/main/java/it/unibo/javadyno/model/data/api/DataTransreciever.java
@@ -4,5 +4,16 @@ package it.unibo.javadyno.model.data.api;
  * DataTransreciever interface.
  */
 public interface DataTransreciever {
+    /**
+     * Retrieves the updated raw data from the data source.
+     * @return the latest raw data
+     */
+    RawData geRawData();
+
+    /**
+     * Sets the data source from which to retrieve the raw data.
+     * @param dataSource the data source to set.
+     */
+    void begin(DataSource dataSource);
 
 }
