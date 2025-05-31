f7adaba126f8cedf4ea05128f008aa13388aa12f
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-05-31 13:40:43 +0200

    Modified Dyno Interface in order to give directly RawData package

--- src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/SimulatedDynoImpl.java
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/SimulatedDynoImpl.java
@@ -1,6 +1,10 @@
 package it.unibo.javadyno.model.dyno.simulated.impl;
 
 import java.util.Objects;
+import java.util.Optional;
+
+import it.unibo.javadyno.model.data.api.RawData;
+import it.unibo.javadyno.model.dyno.simulated.api.Bench;
 import it.unibo.javadyno.model.dyno.simulated.api.SimulatedDyno;
 
 /**
@@ -8,8 +12,14 @@ import it.unibo.javadyno.model.dyno.simulated.api.SimulatedDyno;
  */
 public class SimulatedDynoImpl implements SimulatedDyno, Runnable {
 
+    private static final int ENGINE_RPM = 2000;
+    private static final double ENGINE_TEMPERATURE = 90.0;
+    private static final double ROLLER_INERTIA = 0.01;
+    private static final double ROLLER_RADIUS = 0.1;
     private volatile boolean running;
     private Thread simulationThread;
+    private Bench bench;
+    private volatile RawData datas;
 
     /**
      * Constructor.
@@ -17,6 +27,7 @@ public class SimulatedDynoImpl implements SimulatedDyno, Runnable {
     public SimulatedDynoImpl() {
         this.running = false;
         this.simulationThread = null;
+        this.bench = null;
     }
 
     /**
@@ -25,9 +36,10 @@ public class SimulatedDynoImpl implements SimulatedDyno, Runnable {
     @Override
     public void startSimulation() {
         if (!running) {
-            running = true;
-            simulationThread = new Thread(this);
-            simulationThread.start();
+            this.running = true;
+            this.bench = new BenchImpl(ROLLER_RADIUS, ROLLER_INERTIA);
+            this.simulationThread = new Thread(this);
+            this.simulationThread.start();
         }
     }
 
@@ -38,9 +50,9 @@ public class SimulatedDynoImpl implements SimulatedDyno, Runnable {
     @Override
     public void stopSimulation() {
         if (Objects.nonNull(simulationThread)) {
-            simulationThread.interrupt();
+            this.simulationThread.interrupt();
         }
-        running = false;
+        this.running = false;
     }
 
     /**
@@ -59,43 +71,32 @@ public class SimulatedDynoImpl implements SimulatedDyno, Runnable {
      */
     @Override
     public void run() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'run'");
+        while (this.running) {
+            this.datas = new RawData(
+                    ENGINE_RPM,
+                    ENGINE_TEMPERATURE,
+                    Optional.of(this.bench.getRollerRPM()),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty(),
+                    Optional.empty()
+            );
+            try {
+                Thread.sleep(100);
+            } catch (final InterruptedException e) {
+                this.stopSimulation();
+                break;
+            }
+        }
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public int getEngineRPM() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEngineRPM'");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getRollerRPM() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRollerRPM'");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public double getEngineTemp() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEngineTemp'");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public double getLoadCellValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLoadCellValue'");
+    public RawData getDatas() {
+        return Objects.requireNonNull(this.datas, "RawData not initialized");
     }
 }
