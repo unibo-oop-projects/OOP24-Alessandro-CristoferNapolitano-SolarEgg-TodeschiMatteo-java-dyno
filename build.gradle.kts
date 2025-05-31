c1d5dd04598c4b0f38071f815bda259f9a89889f
Alessandro <alessandroporcheddu000@gmail.com>
2025-05-31 11:10:31 +0200

    added websocket library import

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,6 @@ dependencies {
     // Suppressions for SpotBugs
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.3")
 
-    // Example library: Guava. Add what you need (and use the latest version where appropriate).
-    // implementation("com.google.guava:guava:28.1-jre")
-
     // JavaFX: comment out if you do not need them
     val javaFxVersion = "23.0.2"
     implementation("org.openjfx:javafx:$javaFxVersion")
@@ -45,6 +42,10 @@ dependencies {
         }
     }
 
+    // WebSocket library for external communication
+    val javaWebSocketVersion = "1.6.0"
+    implementation("org.java-websocket:Java-WebSocket:$javaWebSocketVersion")
+    
     // The BOM (Bill of Materials) synchronizes all the versions of Junit coherently.
     testImplementation(platform("org.junit:junit-bom:5.12.2"))
     // The annotations, assertions and other elements we want to have access when compiling our tests.
