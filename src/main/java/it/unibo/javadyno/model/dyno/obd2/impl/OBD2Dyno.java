1cf0ca4dc941bb90a9c23c0e141a74442e4d66b9
Alessandro <alessandroporcheddu000@gmail.com>
2025-06-01 12:50:04 +0200

    added virtual thread support for lighter execution

--- src/main/java/it/unibo/javadyno/model/dyno/obd2/impl/OBD2Dyno.java
+++ src/main/java/it/unibo/javadyno/model/dyno/obd2/impl/OBD2Dyno.java
@@ -1,9 +1,7 @@
 package it.unibo.javadyno.model.dyno.obd2.impl;
 
 import java.util.Optional;
-
 import javax.sql.DataSource;
-
 import it.unibo.javadyno.model.data.api.RawData;
 import it.unibo.javadyno.model.dyno.api.Dyno;
 
@@ -12,7 +10,7 @@ import it.unibo.javadyno.model.dyno.api.Dyno;
  * It retrieve engine RPM, vehicle speed, and other vehicle data from the OBD2 line
  * packed in a RawData object.
  */
-public class OBD2Dyno implements Dyno {
+public class OBD2Dyno implements Dyno, Runnable {
 
     private Optional<Integer> engineRpm;
     private Optional<Integer> vehicleSpeed;
@@ -61,5 +59,24 @@ public class OBD2Dyno implements Dyno {
     public boolean isActive() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isActive'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    /**
+     * Factory method to create a new OBD2Dyno Virtual thread.
+     * Requires Java 21 or --enable-preview option.
+     *
+     * @return a new Thread instance running the OBD2Dyno.
+     */
+    public static Thread make() {
+        return Thread.ofVirtual().unstarted(new OBD2Dyno());
     }
 }
