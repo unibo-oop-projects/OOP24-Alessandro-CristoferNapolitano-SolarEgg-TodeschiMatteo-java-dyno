aa2bec55a25ceea110cef48b4092ff8bafb1e880
Alessandro <alessandroporcheddu000@gmail.com>
2025-06-02 12:46:23 +0200

    created class for USB communication

---
+++ src/main/java/it/unibo/javadyno/model/data/communicator/impl/SerialMCUCommunicator.java
@@ -0,0 +1,66 @@
+package it.unibo.javadyno.model.data.communicator.impl;
+
+import java.util.function.Consumer;
+
+import it.unibo.javadyno.model.data.communicator.api.MCUCommunicator;
+
+/**
+ * Implementation of MCUCommunicator for serial communication with a microcontroller unit (MCU).
+ * It is designed to work over USB with ELM327 compatible MCUs.
+ */
+public class SerialMCUCommunicator implements MCUCommunicator {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void connect() throws InterruptedException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'connect'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void disconnect() throws InterruptedException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'disconnect'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isConnected() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isConnected'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void send(String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'send'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addMessageListener(Consumer<String> listener) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addMessageListener'");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void removeMessageListener(Consumer<String> listener) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeMessageListener'");
+    }
+}
