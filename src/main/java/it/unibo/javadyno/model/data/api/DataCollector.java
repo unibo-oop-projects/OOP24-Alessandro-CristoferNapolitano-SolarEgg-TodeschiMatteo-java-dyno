e6c98190c3678f60f09c3cc2dc653fcc1efde36a
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-06-24 12:01:57 +0200

    Modified DataCollector using Queue

--- src/main/java/it/unibo/javadyno/model/data/api/DataCollector.java
+++ src/main/java/it/unibo/javadyno/model/data/api/DataCollector.java
@@ -1,20 +1,18 @@
 package it.unibo.javadyno.model.data.api;
 
-import java.time.Instant;
-import java.util.List;
+import java.util.Queue;
 
 /**
  * Interface for collecting data.
  */
 public interface DataCollector {
     /**
-     * @return a list of all the datas collected by simulation.
+     * @return a queue of all the datas collected by simulation.
      */
-    List<ElaboratedData> getFullData();
+    Queue<ElaboratedData> getFullData();
 
     /**
-     * @param timestamp the time instant to retrieve data for.
-     * @return data of a specified time instant by simulation.
+     * Collect the last data collected by simulation.
      */
-    ElaboratedData getInstantData(Instant timestamp);
+    void collectData();
 }
