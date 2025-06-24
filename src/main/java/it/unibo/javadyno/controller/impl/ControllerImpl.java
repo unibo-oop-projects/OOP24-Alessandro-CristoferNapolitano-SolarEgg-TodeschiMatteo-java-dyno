144fe2da3efea955f677d65ceb3850231657041f
TodeschiMatteo <matteo.todeschi2@studio.unibo.it>
2025-06-24 12:56:03 +0200

    Added Dyno isntance in DataTransreceiver.begin() and updated startSimulation in controller

--- src/main/java/it/unibo/javadyno/controller/impl/ControllerImpl.java
+++ src/main/java/it/unibo/javadyno/controller/impl/ControllerImpl.java
@@ -3,6 +3,10 @@ package it.unibo.javadyno.controller.impl;
 import java.util.Objects;
 
 import it.unibo.javadyno.controller.api.Controller;
+import it.unibo.javadyno.model.data.api.DataCollector;
+import it.unibo.javadyno.model.data.api.DataSource;
+import it.unibo.javadyno.model.data.api.DataTransreciever;
+import it.unibo.javadyno.model.data.impl.DataCollectorImpl;
 import it.unibo.javadyno.model.dyno.api.Dyno;
 import it.unibo.javadyno.model.dyno.simulated.impl.SimulatedDynoImpl;
 import it.unibo.javadyno.view.impl.ViewImpl;
@@ -14,12 +18,16 @@ import javafx.application.Application;
 public class ControllerImpl implements Controller {
 
     private Dyno dyno;
+    private final DataCollector dataCollector;
+    private final DataTransreciever dataTransreciever;
 
     /**
      * Default constructor for ControllerImpl.
      */
     public ControllerImpl() {
         this.dyno = null;
+        this.dataCollector = new DataCollectorImpl();
+        this.dataTransreciever = null;
     }
 
     /**
@@ -44,6 +52,8 @@ public class ControllerImpl implements Controller {
     public void startSimulation() {
         if (!this.dyno.isActive()) {
             this.dyno = new SimulatedDynoImpl();
+            this.dataCollector.clearData();
+            this.dataTransreciever.begin(DataSource.SIMULATED_DYNO, this.dyno);
             this.dyno.begin();
         } else {
             throw new IllegalStateException("Simulation is already running."); // maybe alert box?
--- src/main/java/it/unibo/javadyno/model/data/api/DataTransreciever.java
+++ src/main/java/it/unibo/javadyno/model/data/api/DataTransreciever.java
@@ -1,5 +1,7 @@
 package it.unibo.javadyno.model.data.api;
 
+import it.unibo.javadyno.model.dyno.api.Dyno;
+
 /**
  * DataTransreciever interface.
  */
@@ -14,8 +16,9 @@ public interface DataTransreciever {
     /**
      * Sets the data source from which to retrieve the raw data.
      *
-     * @param dataSource the data source to set.
+     * @param dataSource the data source type to set.
+     * @param dyno the dyno instance to associate with the data source
      */
-    void begin(DataSource dataSource);
+    void begin(DataSource dataSource, Dyno dyno);
 
 }
