1821d3acb5d5674f31c6d2ee0d671f6daa468ec5
cristoferNapolitano <cristofer.napolitano@gmail.com>
2025-06-25 19:36:10 +0200

    creation of a Vehicle implementation

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/VehicleImpl.java
@@ -0,0 +1,56 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import it.unibo.javadyno.model.dyno.simulated.api.Vehicle;
+import it.unibo.javadyno.model.dyno.simulated.api.WeatherStation;
+
+public class VehicleImpl implements Vehicle{
+    private final RigidDriveTrainSim drivetrain;
+    private final WeatherStation weather;
+    private final double wheelRadius;
+    private double currentThrottle;
+
+    /**
+     * 
+     * @param drivetrain
+     * @param weather
+     * @param wheelRadius wheel radius, used to compute vehicle speed [m]
+     * @param currentThrottle
+     */
+    public VehicleImpl(RigidDriveTrainSim drivetrain, WeatherStation weather,
+            double wheelRadius, double currentThrottle) {
+        this.drivetrain   = java.util.Objects.requireNonNull(drivetrain, "drivetrain");
+        this.weather      = java.util.Objects.requireNonNull(weather,  "weather");
+        if (wheelRadius <= 0) throw new IllegalArgumentException("wheelRadius must be > 0");
+        this.wheelRadius  = wheelRadius;
+    }
+
+    @Override
+    public void setThrottle(double throttle) {
+        if (throttle < 0.0 || throttle > 1.0) {
+            throw new IllegalArgumentException("throttle must be between [0-1]");
+        }
+        this.currentThrottle = throttle;
+    }
+
+    @Override
+    public void update(double deltatime) {
+        drivetrain.step(currentThrottle,deltatime);
+    }
+
+    @Override
+    public void shiftUp() {
+        drivetrain.shiftUp();
+    }
+
+    @Override
+    public void shiftDown() {
+        drivetrain.shiftDown();
+    }
+
+    /* TODO :
+    VehicleRawData getVehicleRawData() {
+
+    }
+    */
+    
+}
