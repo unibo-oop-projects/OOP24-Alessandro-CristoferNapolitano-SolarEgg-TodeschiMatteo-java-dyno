84e18fbd2a821c7aa3c1d642983cd38b5c7fe9b6
cristoferNapolitano <cristofer.napolitano@gmail.com>
2025-06-25 19:35:10 +0200

    creation of a Rigid version of a driveTrain

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/RigidDriveTrainSim.java
@@ -0,0 +1,70 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import java.util.List;
+import java.util.Objects;
+
+import it.unibo.javadyno.model.dyno.simulated.api.DriveTrain;
+import it.unibo.javadyno.model.dyno.simulated.api.Engine;
+import it.unibo.javadyno.model.dyno.simulated.api.LoadModel;
+import it.unibo.javadyno.model.dyno.simulated.api.Transmission;
+
+public class RigidDriveTrainSim implements DriveTrain{
+    private final Engine engine;
+    private final Transmission transmission;
+    private final List<LoadModel> loads;
+    private final double deltaTime;
+
+    public RigidDriveTrainSim(Engine engine, Transmission transmission, List<LoadModel> loads,
+            double deltaTime) {
+        this.engine = Objects.requireNonNull(engine);
+        this.transmission = Objects.requireNonNull(transmission);
+        this.loads = List.copyOf(loads);
+        if (deltaTime <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.deltaTime = deltaTime;
+    }
+
+    @Override
+    public void step(double throttle, double deltatime) {
+        engine.setThrottle(throttle);
+        double engineOmega = engine.getAngularVelocity();
+        double gearRatio = transmission.getCurrentRatio();
+        double totalLoad = loads.stream()
+            .mapToDouble(lm -> lm.getLoadTorque(engineOmega, gearRatio))
+            .sum();
+        engine.update(totalLoad, deltaTime);
+    }
+    
+    public double getEngineOmega() {
+        return engine.getAngularVelocity();
+    }
+
+    public double getWheelOmega() {
+        return engine.getAngularVelocity() * transmission.getCurrentRatio();
+    }
+
+    //TODO : creation of a clutch component to synchronize engine and wheel speed
+    public void shiftDown() {
+        double oldRatio  = transmission.getCurrentRatio();
+        double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
+
+        transmission.shiftUp();
+
+        double newRatio     = transmission.getCurrentRatio();
+        double newEngineOmega   = oldWheelOmega / newRatio;
+        engine.setAngularVelocity(newEngineOmega);
+    }
+
+    //TODO : creation of a clutch component to synchronize engine and wheel speed
+    public void shiftUp() {
+        double oldRatio  = transmission.getCurrentRatio();
+        double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
+
+        transmission.shiftDown();
+
+        double newRatio     = transmission.getCurrentRatio();
+        double newEngineOmega   = oldWheelOmega / newRatio;
+        engine.setAngularVelocity(newEngineOmega);
+    }
+}
