7b4a83a14130cf82ecfcdde2e5e6a239724f12f4
cristoferNapolitano <cristofer.napolitano@gmail.com>
2025-06-26 22:17:40 +0200

    creating and implementing TemperatureModel and SimpleTenperatureModel

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/SimpleTemperatureModel.java
@@ -0,0 +1,19 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import it.unibo.javadyno.model.dyno.simulated.api.TemperatureModel;
+
+public class SimpleTemperatureModel implements TemperatureModel{
+
+    @Override
+    public void update(double generatedPower, double ambientTemp, double deltaTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public double getTemperature() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTemperature'");
+    }
+    
+}
