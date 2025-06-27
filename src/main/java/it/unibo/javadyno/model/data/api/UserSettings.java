a797b76757511d3fd1a142b9a20d0af9056930c3
Alessandro <alessandroporcheddu000@gmail.com>
2025-06-27 11:40:48 +0200

    implemented OBD2 power calculations

--- src/main/java/it/unibo/javadyno/model/data/api/UserSettings.java
+++ src/main/java/it/unibo/javadyno/model/data/api/UserSettings.java
@@ -5,7 +5,13 @@ package it.unibo.javadyno.model.data.api;
  */
 public enum UserSettings {
     SIMULATION_UPDATE_TIME_DELTA(100),
-    LOADCELL_LEVER_LENGTH(0.5);
+    LOADCELL_LEVER_LENGTH(0.5),
+    VEHICLE_MASS(1500),
+    ROLLING_RESISTANCE_COEFFICIENT(0.012),
+    AIR_DRAG_COEFFICIENT(0.32),
+    FRONTAL_AREA(2.2),
+    AIR_DENSITY(1.225),
+    DRIVE_TRAIN_EFFICIENCY(0.85);
 
     private final Object defaultValue;
 
--- src/main/java/it/unibo/javadyno/model/data/impl/DataElaboratorImpl.java
+++ src/main/java/it/unibo/javadyno/model/data/impl/DataElaboratorImpl.java
@@ -14,8 +14,11 @@ import it.unibo.javadyno.model.dyno.api.Dyno;
  */
 public final class DataElaboratorImpl implements DataElaborator {
 
-    private static final double KW_TO_HP_DIVISOR = 1.3596;
+    private static final double HEARTH_GRAVITY_ACCELERATION = 9.81;
+    private static final double KMH_TO_MS = 3.6;
+    private static final double KW_TO_HP_MULTIPLIER = 1.3596;
     private static final int ENGINE_POWER_KW_DIVISOR = 9549;
+    private RawData prevRawData;
     private final Dyno dyno;
     private final DataSource dataSource;
 
@@ -56,24 +59,56 @@ public final class DataElaboratorImpl implements DataElaborator {
      * @return an ElaboratedData object containing the processed data
      */
     private ElaboratedData processOBD2Data(final RawData rawData) {
-        //TODO Implement the logic to elaborate the raw data
+        if (!rawData.vehicleSpeed().isPresent()
+            || !rawData.engineRPM().isPresent()
+            || !rawData.timestamp().isPresent()) {
+            throw new IllegalStateException("Raw data must contain both torque and RPM values.");
+            // tell alert monitor
+        } else if (Objects.isNull(prevRawData)) {
+            prevRawData = rawData;
+            return null;
+        }
+        final Integer vehicleSpeedDelta = rawData.vehicleSpeed().get() - prevRawData.vehicleSpeed().get();
+        final long timeDelta = rawData.timestamp().get().toEpochMilli()
+            - prevRawData.timestamp().orElseThrow().toEpochMilli();
+        if (vehicleSpeedDelta <= 0) {
+            throw new IllegalStateException("Vehicle speed cannot decrease in OBD2 data processing.");
+            // tell alert monitor
+        }
+        final Double acceleration = vehicleSpeedDelta / KMH_TO_MS / timeDelta;
+        final Double inertialForce = acceleration * (Double) UserSettings.VEHICLE_MASS.getDefaultValue();
+        final Double rollingResistanceForce = (Double) UserSettings.ROLLING_RESISTANCE_COEFFICIENT.getDefaultValue()
+            * (Double) UserSettings.VEHICLE_MASS.getDefaultValue() * HEARTH_GRAVITY_ACCELERATION;
+        final Double aerodynamicDragForce = 0.5
+            * (Double) UserSettings.AIR_DRAG_COEFFICIENT.getDefaultValue()
+            * (Double) UserSettings.FRONTAL_AREA.getDefaultValue()
+            * (Double) UserSettings.AIR_DENSITY.getDefaultValue()
+            * Math.pow(rawData.vehicleSpeed().get() / KMH_TO_MS, 2);
+        final Double totalForce = inertialForce + rollingResistanceForce + aerodynamicDragForce;
+        final Double enginePowerKW = totalForce * rawData.vehicleSpeed().get() / KMH_TO_MS
+            / (Double) UserSettings.DRIVE_TRAIN_EFFICIENCY.getDefaultValue();
+        final Double enginePowerHP = enginePowerKW * KW_TO_HP_MULTIPLIER;
+        final Double engineCorrectedTorque =
+            enginePowerKW * ENGINE_POWER_KW_DIVISOR / rawData.engineRPM().orElseThrow();
+        prevRawData = rawData;
         return new ElaboratedData(
             rawData,
-            0.0,
-            0.0,
-            0.0
+            enginePowerKW,
+            enginePowerHP,
+            engineCorrectedTorque
         );
     }
 
     private ElaboratedData processDynoData(final RawData rawData) {
         if (!rawData.torque().isPresent() || !rawData.engineRPM().isPresent()) {
             throw new IllegalStateException("Raw data must contain both torque and RPM values.");
+            // tell alert monitor
         }
         final Double engineCorrectedTorque =
             rawData.torque().get()
             * (Double) UserSettings.LOADCELL_LEVER_LENGTH.getDefaultValue();
         final Double enginePowerKW = engineCorrectedTorque * rawData.engineRPM().get() / ENGINE_POWER_KW_DIVISOR;
-        final Double enginePowerHP = enginePowerKW * KW_TO_HP_DIVISOR;
+        final Double enginePowerHP = enginePowerKW * KW_TO_HP_MULTIPLIER;
         return new ElaboratedData(
             rawData,
             enginePowerKW,
