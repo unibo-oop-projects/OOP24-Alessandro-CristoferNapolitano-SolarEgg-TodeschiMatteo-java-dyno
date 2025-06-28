ee37a4099ee786fe0e5ee3f943a0964f157d948f
cristoferNapolitano <cristofer.napolitano@gmail.com>
2025-06-28 14:46:31 +0200

    implementing BrakeTorqueProvider: BenchBrakeTorqueProvider

---
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/BenchBrakeTorqueHolder.java
@@ -0,0 +1,31 @@
+package it.unibo.javadyno.model.dyno.simulated.impl;
+
+import it.unibo.javadyno.model.dyno.simulated.api.BrakeTorqueProvider;
+
+/**
+ * a mutable holder of brake torque power for the BenchLoad.
+ */
+public class BenchBrakeTorqueHolder implements BrakeTorqueProvider{
+    private double currentBrakeTorque;
+    
+    /**
+     * {@inheritDoc }
+     */
+    @Override
+    public double getBrakeTorque() {
+        return currentBrakeTorque;
+    }
+
+    /**
+     * setter for bench brake torque value must be >= 0.
+     *
+     * @param benchBrakeTorque bench brake torque, value must be >= 0
+     */
+    public void setBrakeTorque(double benchBrakeTorque) {
+        if (benchBrakeTorque < 0) {
+            throw new IllegalArgumentException("brake torque must be >= 0");
+        }
+        this.currentBrakeTorque = benchBrakeTorque;
+    }
+    
+}
