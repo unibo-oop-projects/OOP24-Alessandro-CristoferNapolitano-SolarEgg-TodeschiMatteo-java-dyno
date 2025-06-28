2061ff6fd91bfc67c9cc658effb7c4676c712122
SolarEgg <ivcrima@gmail.com>
2025-06-28 21:33:04 +0200

    Added a Factory (to handle choosing the correct Strategy based on the given file extention)

---
+++ src/main/java/it/unibo/javadyno/model/filemanager/api/FileStrategyFactory.java
@@ -0,0 +1,19 @@
+package it.unibo.javadyno.model.filemanager.api;
+
+import java.io.File;
+import java.util.Optional;
+
+/**
+ * An interface for a factory that creates FileStrategy objects.
+ */
+public interface FileStrategyFactory {
+
+    /**
+     * Creates a FileStrategy based on the provided file's extension.
+     *
+     * @param file The file for which to choose and create a strategy.
+     * @return An Optional containing the appropriate FileStrategy if a supported
+     *         extension is found, otherwise an empty Optional.
+     */
+    Optional<FileStrategy> createStrategyFor(File file);
+}
