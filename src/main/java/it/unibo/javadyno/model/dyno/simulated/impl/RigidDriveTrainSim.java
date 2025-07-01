2ed22a60f706661dd259e4558b22c23aa4f9e2e8
cristoferNapolitano <cristofer.napolitano@gmail.com>
2025-07-01 12:39:08 +0200

    removing deltaTime, syntax correction to RidigDriveTrain

--- src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/RigidDriveTrainSim.java
+++ src/main/java/it/unibo/javadyno/model/dyno/simulated/impl/RigidDriveTrainSim.java
@@ -8,71 +8,103 @@ import it.unibo.javadyno.model.dyno.simulated.api.Engine;
 import it.unibo.javadyno.model.dyno.simulated.api.LoadModel;
 import it.unibo.javadyno.model.dyno.simulated.api.Transmission;
 
-public class RigidDriveTrainSim implements DriveTrain{
+/**
+ * implementation that holds and manages: 
+ * Engine, Transmission, list of LoadModel, deltaTime.
+ */
+public class RigidDriveTrainSim implements DriveTrain {
     private final Engine engine;
     private final Transmission transmission;
     private final List<LoadModel> loads;
-    private final double deltaTime;
 
-    public RigidDriveTrainSim(Engine engine, Transmission transmission, List<LoadModel> loads,
-            double deltaTime) {
+    /**
+     * simple constructor for RigidDriveTrainSim.
+     *
+     * @param engine non null Engine
+     * @param transmission non null Transmission
+     * @param loads list of LoadModel for the drive train
+     */
+    public RigidDriveTrainSim(final Engine engine, final Transmission transmission, final List<LoadModel> loads) {
         this.engine = Objects.requireNonNull(engine);
         this.transmission = Objects.requireNonNull(transmission);
         this.loads = List.copyOf(loads);
-        if (deltaTime <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.deltaTime = deltaTime;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void step(double throttle, double deltatime) {
+    public void step(final double throttle, final double deltaTime) {
         engine.setThrottle(throttle);
-        double engineOmega = engine.getAngularVelocity();
-        double gearRatio = transmission.getCurrentRatio();
-        double totalLoad = loads.stream()
+        final double engineOmega = engine.getAngularVelocity();
+        final double gearRatio = transmission.getCurrentRatio();
+        final double totalLoad = loads.stream()
             .mapToDouble(lm -> lm.getLoadTorque(engineOmega, gearRatio))
             .sum();
         engine.update(totalLoad, deltaTime);
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getEngineOmega() {
         return engine.getAngularVelocity();
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getEngineTemperature() {
         return engine.getEngineTemperature();
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getWheelOmega() {
         return engine.getAngularVelocity() * transmission.getCurrentRatio();
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public double getGeneratedTorque() {
         return engine.getGeneratedTorque();
     }
 
-    //TODO : creation of a clutch component to synchronize engine and wheel speed
+    /**
+     * {@inheritDoc}
+     * //TODO : creation of a clutch component to synchronize engine and wheel speed
+     */
+    @Override
     public void shiftDown() {
-        double oldRatio  = transmission.getCurrentRatio();
-        double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
+        final double oldRatio = transmission.getCurrentRatio();
+        final double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
 
         transmission.shiftUp();
 
-        double newRatio     = transmission.getCurrentRatio();
-        double newEngineOmega   = oldWheelOmega / newRatio;
+        final double newRatio = transmission.getCurrentRatio();
+        final double newEngineOmega = oldWheelOmega / newRatio;
         engine.setAngularVelocity(newEngineOmega);
     }
 
-    //TODO : creation of a clutch component to synchronize engine and wheel speed
+    /**
+     * {@inheritDoc}
+     * //TODO : creation of a clutch component to synchronize engine and wheel speed
+     */
+    @Override
     public void shiftUp() {
-        double oldRatio  = transmission.getCurrentRatio();
-        double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
+        final double oldRatio = transmission.getCurrentRatio();
+        final double oldWheelOmega = engine.getAngularVelocity() * oldRatio;
 
         transmission.shiftDown();
 
-        double newRatio     = transmission.getCurrentRatio();
-        double newEngineOmega   = oldWheelOmega / newRatio;
+        final double newRatio = transmission.getCurrentRatio();
+        final double newEngineOmega = oldWheelOmega / newRatio;
         engine.setAngularVelocity(newEngineOmega);
     }
 
